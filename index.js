ca3175509875f42a2dfa7509085407fab54794ed
ali <ali0936781321@gmail.com>
2024-11-20 23:42:17 +0300

    The final touch

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const side = document.querySelector('aside');
+const men = document.getElementById('menu-btn');
+const close = document.getElementById('close-btn');
+
+const dark = document.querySelector('.dark-mode');
+
+men.addEventListener('click', () => {
+ side.style.display = 'block';
+}); 
+
+close.addEventListener('click', () => {
+ side.style.display = 'none';
+});
+
+
+dark.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode-variables');
+    dark.querySelector('span:nth-child(1)').classList.toggle('active');
+    dark.querySelector('span:nth-child(2)').classList.toggle('active');
+})
+
 Orders.forEach(order => {
     const tr = document.createElement('tr');
     const trContent = ` 
